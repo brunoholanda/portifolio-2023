3b6aec140a5b7a23735fa987fe80e16e462f7e38
brunoholanda <holanda_rodrigues@hotmail.com>
2023-05-12 18:37:29 -0300

    correcao do bug nos links e novo link para CV

--- src/components/SocialLinks/index.jsx
+++ src/components/SocialLinks/index.jsx
@@ -3,13 +3,13 @@ import styles from './SocialLinks.module.scss';
 export default function SocialLinks() {
     return (
         <div className={styles.socialLinks}>
-            <a href="https://github.com/brunoholanda" target="_blank">
+            <a href="https://github.com/brunoholanda" target="_blank" rel="noreferrer">
             <div className={styles.socialLinks__git}></div>
             </a>
-            <a href="https://www.linkedin.com/in/brunoholanda/" target="_blank">
+            <a href="https://www.linkedin.com/in/brunoholanda/" target="_blank" rel="noreferrer">
             <div className={styles.socialLinks__li}></div>
             </a>
-            <a href="https://wa.me/5583998150712" target="_blank">
+            <a href="https://wa.me/5583998150712" target="_blank" rel="noreferrer">
                 <div className={styles.socialLinks__wp}></div>
             </a>
         </div>
